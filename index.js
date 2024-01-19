eb4fbe9e1a200d7c8901c2c7f9c221de9d7de4ca
Moneshai2004 <moneshshanmugam@gmail.com>
2024-01-19 18:56:16 +0530

    fnal

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+const accesskey = "fzwaE59O8ReyY2kXYtkaLBA4_P8YfapRNqYJ_0W4rLU";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const showMorebtn = document.getElementById("show-more-btn-form");
+const showMoreBtn = document.getElementById("show-more-btn-form");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  results.forEach((image) => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image.urls.small;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = image.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(imgElement);
+    searchResult.appendChild(imageLink);
+  });
+}
+
+function loadMoreImages() {
+  page++;
+  searchImages();
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchResult.innerHTML = ""; // Clear previous results
+  searchImages();
+});
+
+showMoreBtn.addEventListener("click", loadMoreImages);
